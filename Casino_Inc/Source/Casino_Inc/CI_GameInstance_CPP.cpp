28281629515f426e4fde0761c01cd37acb2ac384
matthieu1345-Desktop <matthieuberg@hotmail.com>
2019-11-27 04:57:20 +0100

    setup usage of sound system

---
+++ Casino_Inc/Source/Casino_Inc/CI_GameInstance_CPP.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CI_GameInstance_CPP.h"
+#include "Kismet/GameplayStatics.h"
+
+UCI_GameInstance_CPP::UCI_GameInstance_CPP(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
---
+++ Casino_Inc/Source/Casino_Inc/CI_GameInstance_CPP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CI_GameInstance_CPP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASINO_INC_API UCI_GameInstance_CPP : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UCI_GameInstance_CPP(const FObjectInitializer& ObjectInitializer);
+};
