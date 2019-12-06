d8a3a88d187a8939a43ae03bb8c7f9567e73ad52
matthieu1345-Desktop <matthieuberg@hotmail.com>
2019-12-07 00:09:18 +0100

    commented the BaseAIController files
    commented the BaseAIController files

--- Casino_Inc/Source/Casino_Inc/AI/Base/CI_BaseAIController_CPP.cpp
+++ Casino_Inc/Source/Casino_Inc/AI/Base/CI_BaseAIController_CPP.cpp
@@ -9,15 +9,15 @@
 #include "TileMap/CI_TileMapCoordinateMath.h"
 #include "TimerManager.h"
 
-//TODO:DOCUMENT comment/document this file
-
 ACI_BaseAIController_CPP::ACI_BaseAIController_CPP()
 {
+	//set default controller values
 	bAttachToPawn = true;
 	bIsPlayerController = false;
 	IgnoreLookInput = true;
 	IgnoreMoveInput = false;
 
+	// Create the root component, and add a pathfollowing and goapWorker component
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	pathFollowingComp = CreateDefaultSubobject<UCI_PathFollowingComponent_CPP>(TEXT("PathFollowingComponent"));
 	AddOwnedComponent(pathFollowingComp);
@@ -28,57 +28,58 @@ ACI_BaseAIController_CPP::ACI_BaseAIController_CPP()
 
 ACI_BaseAIController_CPP::~ACI_BaseAIController_CPP()
 {
-	/*if (IsValid(pathFollowingComp))
-		pathFollowingComp->DestroyComponent();
-	if (IsValid(goapWorkerComponent))
-		goapWorkerComponent->DestroyComponent*/
 }
 
 void ACI_BaseAIController_CPP::RegisterAI()
 {
+	//register with the AI Manager
 	ACI_AIManager_CPP::GetInstance(GetWorld())->RegisterAI(possessedCustomBasePawn, this);
 }
 
 void ACI_BaseAIController_CPP::GetNewTask()
 {
+	// Get a new task from the AI manager
 	ACI_AIManager_CPP::GetInstance(GetWorld())->GetNextTask(type, currentTask);
 
-	if (currentTask != nullptr)
+	if (currentTask != nullptr) //if there's actually a task for this ai
 	{
-		goapWorkerComponent->StartNewPlan(currentTask);
+		goapWorkerComponent->StartNewPlan(currentTask); //create a plan to do the task
 	}
 	else
 	{
-		DoIdle();
+		DoIdle(); // do the ai's idle behaviour
 	}
 }
 
 void ACI_BaseAIController_CPP::TaskEnded_Implementation(bool success)
 {
 	if (success)
-		ACI_AIManager_CPP::GetInstance(GetWorld())->FinishTask(currentTask);
+		ACI_AIManager_CPP::GetInstance(GetWorld())->FinishTask(currentTask); //send to the AIManager that the task has ended succesfully and needs to be finished
 	else
-		ACI_AIManager_CPP::GetInstance(GetWorld())->CancelTask(currentTask);
+		ACI_AIManager_CPP::GetInstance(GetWorld())->CancelTask(currentTask); //send to the AIManager that the task has ended unsuccesful and needs to be canceled
 
-	currentTask = nullptr;
+	currentTask = nullptr; //reset the task this ai is doing to "none"
 }
 
 void ACI_BaseAIController_CPP::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// Register the goapWorker component's callback functions
 	goapWorkerComponent->startPath.RemoveDynamic(pathFollowingComp, &UCI_PathFollowingComponent_CPP::GetNewPath);
 	goapWorkerComponent->startPath.AddDynamic(pathFollowingComp, &UCI_PathFollowingComponent_CPP::GetNewPath);
 	goapWorkerComponent->finishedCallback.RemoveDynamic(this, &ACI_BaseAIController_CPP::TaskEnded);
 	goapWorkerComponent->finishedCallback.AddDynamic(this, &ACI_BaseAIController_CPP::TaskEnded);
 
+	// Register the pathFollowing component's callback functions
 	pathFollowingComp->pathFinishedDelegate.RemoveDynamic(goapWorkerComponent, &UCI_GOAPWorkerComponent_CPP::PathFinished);
 	pathFollowingComp->pathFinishedDelegate.AddDynamic(goapWorkerComponent, &UCI_GOAPWorkerComponent_CPP::PathFinished);
 
+	// Register the TimeManager's hourly callback function
 	ACI_TimeManager_CPP::GetInstance(GetWorld())->nextHourDelegate.AddDynamic(this, &ACI_BaseAIController_CPP::HourChanged);
 }
 
-void ACI_BaseAIController_CPP::Tick(float DeltaSeconds) 
+void ACI_BaseAIController_CPP::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 }
@@ -86,17 +87,22 @@ void ACI_BaseAIController_CPP::Tick(float DeltaSeconds)
 void ACI_BaseAIController_CPP::Possess(APawn* InPawn)
 {
 	Super::Possess(InPawn);
-
-
-	if (InPawn->GetClass()->IsChildOf(ACI_BaseAIPawn_CPP::StaticClass()))
+	
+	if (InPawn->GetClass()->IsChildOf(ACI_BaseAIPawn_CPP::StaticClass())) // if the possessed pawn is a ACI_BaseAIPawn_CPP
 	{
+		// keep a casted reference (casting is heavy so this is to keep that to a single time)
 		possessedCustomBasePawn = Cast<ACI_BaseAIPawn_CPP>(InPawn);
 
+		// Register the goapWorker component's callback functions to the pawn
 		goapWorkerComponent->stateChangedCallback.AddDynamic(possessedCustomBasePawn, &ACI_BaseAIPawn_CPP::StateChanged);
+
+		// Register the ai to the manager in the next tick
 		GetWorldTimerManager().SetTimerForNextTick(this, &ACI_BaseAIController_CPP::RegisterAI);
 
+		// set the ai's name
 		possessedCustomBasePawn->aiName = name;
 
+		//call the "HourChanged" function once to do any initial checks based on time
 		HourChanged(ACI_TimeManager_CPP::GetInstance(GetWorld())->GetCurrentTime().Y);
 	}
 }
@@ -105,6 +111,7 @@ void ACI_BaseAIController_CPP::UnPossess()
 {
 	Super::UnPossess();
 
+	// un-register the ai to the ai manager
 	if (ACI_AIManager_CPP::GetInstance(GetWorld()).IsValid())
 		ACI_AIManager_CPP::GetInstance(GetWorld())->RemoveAI(possessedCustomBasePawn, type);
 
@@ -119,17 +126,18 @@ void ACI_BaseAIController_CPP::EnterMap()
 
 void ACI_BaseAIController_CPP::LeaveMap()
 {
-	isAway = true;
+	isAway = true; // set the isAway flag
 
-	if (goapWorkerComponent->IsActiveState())
+	if (goapWorkerComponent->IsActiveState()) // stop the task that the ai is currently doing
 	{
 		goapWorkerComponent->CancelPlan();
 	}
 
-	if (pathFollowingComp->IsActiveState())
+	if (pathFollowingComp->IsActiveState()) // stop the path the ai is currently following
 	{
 		pathFollowingComp->CancelPath();
 	}
 
+	// Get a new path towards a leaving location
 	pathFollowingComp->GetNewPath(UCI_TileMapCoordinateMath::WorldVectorToTile(possessedCustomBasePawn->GetActorLocation()), ACI_AIManager_CPP::GetInstance(GetWorld())->GetRandomSpawnTile());
 }
--- Casino_Inc/Source/Casino_Inc/AI/Base/CI_BaseAIController_CPP.h
+++ Casino_Inc/Source/Casino_Inc/AI/Base/CI_BaseAIController_CPP.h
@@ -7,10 +7,10 @@
 #include "AI/CI_AITypes.h"
 #include "CI_BaseAIController_CPP.generated.h"
 
-//TODO:DOCUMENT comment/document this file
-
 /**
- * 
+ * Base AI controllers for <b>Casino Inc.</b><br>
+ * has some basic implementation for leaving/entering the casino, and implementable functions for behaviour<br>
+ * includes pathfinder and goapWorker components
  */
 UCLASS()
 class CASINO_INC_API ACI_BaseAIController_CPP : public AController
@@ -22,57 +22,144 @@ public:
 		ACI_BaseAIController_CPP();
 	~ACI_BaseAIController_CPP();
 
+	/**
+	 *	\brief Function called every frame on this Actor. Override this function to implement custom logic to be executed every frame.
+	 *	\param DeltaSeconds	Game time elapsed during last frame modified by the time dilation
+	 */
 	virtual void Tick(float DeltaSeconds) override;
+
+	/**
+	 * \brief Handles attaching this controller to the specified pawn.
+	 * \param InPawn The Pawn to be possessed.
+	 */
 	virtual void Possess(APawn* InPawn) override;
+
+	/** 
+	 * \brief Called to unpossess our pawn for any reason that is not the pawn being destroyed (destruction handled by PawnDestroyed()). 
+	 */
 	virtual void UnPossess() override;
 
-
+	/**
+	 * \return The Pathfollowing Component
+	 */
 	UFUNCTION(BlueprintGetter)
 		class UCI_PathFollowingComponent_CPP* GetPathFollowingComponent() const { return pathFollowingComp; }
+	/**
+	 * \return The possessed BasePawn
+	 */
 	UFUNCTION(BlueprintGetter)
 		class ACI_BaseAIPawn_CPP* GetBasePawn() const { return possessedCustomBasePawn; }
 
+	/**
+	 * \brief Register the ai to the AIManager
+	 */
 	UFUNCTION()
 		void RegisterAI();
 
+	/**
+	 * \return The type of the character
+	 */
 	UFUNCTION(BlueprintGetter)
 		ECharacterType GetType() const { return type; }
 
+	/**
+	 * \brief Ask for a new task from the AIManager
+	 */
 	virtual void GetNewTask();
 
+	/**
+	 * \brief return here if the task has ended
+	 * \param success input if the task was a success or a fail
+	 */
 	UFUNCTION()
 		void TaskEndCall(bool success) { TaskEnded(success); }
 
+	/**
+	 * \brief return here if the task has ended
+	 * \param success input if the task was a success or a fail
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
 		void TaskEnded(bool success);
+
+	/**
+	 * \brief c++ implementation for <b>TaskEnded</b>:<br>
+	 * return here if the task has ended
+	 * Defaults to asking for a new task from the ai manager, or go to the idle behavior if none available
+	 * \param success input if the task was a success or a fail
+	 */
 	virtual void TaskEnded_Implementation(bool success);
 
 protected:
+	/**
+	 *\brief native event for when play begins for this actor.
+	 */
 	virtual void BeginPlay() override;
+
+	/**
+	 * \brief Implement to give the character a idle behaviour
+	 */
 	virtual void DoIdle(){};
 
+	/**
+	 * \brief A function that get's called every time the ingame hour changes<br>
+	 * implement this to give the ai custom behaviour that needs to be done hourly
+	 */
 	UFUNCTION()
 	virtual void HourChanged(int hourNumber){}
 
+
+	/**
+	 * \brief called when the ai needs to enter the map<br>
+	 * makes the character visible and resets the idle and away booleans
+	 */
 	virtual void EnterMap();
+
+	/**
+	 * \brief called when the ai needs to Leave the map<br>
+	 * makes the character move towards a leaving spot and set's the idle and away booleans
+	 */
 	virtual void LeaveMap();
 
 protected:
+	/**
+	 * \brief this ai's path-following component
+	 */
 	UPROPERTY(BlueprintReadOnly, Instanced, EditAnywhere)
 		class UCI_PathFollowingComponent_CPP* pathFollowingComp;
+	/**
+	 * \brief this ai's goap worker component
+	 */
 	UPROPERTY(BlueprintReadOnly, Instanced, EditAnywhere)
 		class UCI_GOAPWorkerComponent_CPP* goapWorkerComponent;
 
+	/**
+	 * \brief this ai's possesed pawn as <b>ACI_BaseAIPawn_CPP</b> pointer
+	 */
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
 		class ACI_BaseAIPawn_CPP* possessedCustomBasePawn = nullptr;
 
+	/**
+	 * \brief this ai's character type
+	 */
 	ECharacterType type = ECharacterType::CT_Invalid;
 
+	/**
+	 * \brief this ai's current task
+	 */
 	struct FTask* currentTask;
+	/**
+	 * \brief if the ai is currently doing a task
+	 */
 	bool doingTask = false;
 
+	/**
+	 * \brief if the ai is away from/leaving the casino
+	 */
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "state")
 		bool isAway = true;
 
+	/**
+	 * \brief the name of the ai
+	 */
 	FString name = "AI";
 };
