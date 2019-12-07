2a5ad6a9b09be68f48be4b63d5ebf8922f51e85f
matthieu1345-Desktop <matthieuberg@hotmail.com>
2019-12-07 21:00:48 +0100

    ~ dealer controller documentation and comments
    added documentation and comments to the dealer controller code

--- Casino_Inc/Source/Casino_Inc/AI/Characters/CI_DealerController_CPP.cpp
+++ Casino_Inc/Source/Casino_Inc/AI/Characters/CI_DealerController_CPP.cpp
@@ -13,13 +13,11 @@
 #include "TileMap/CI_TileMapCoordinateMath.h"
 #include "TileMap/CI_BaseTile_CPP.h"
 
-//TODO:DOCUMENT comment/document this file
-
 ACI_DealerController_CPP::ACI_DealerController_CPP()
 {
+	// set dealer specific default variables
 	type = ECharacterType::CT_Dealer;
 	pathFollowingComp->EnableMovement();
-
 	name = "Dealer";
 }
 
@@ -28,6 +26,7 @@ void ACI_DealerController_CPP::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
+	//if we don't have a task, are idle, and are at the casino, ask for a new task
 	if (currentTask == nullptr && isIdle && !isAway)
 		GetNewTask();
 }
@@ -42,20 +41,24 @@ void ACI_DealerController_CPP::DoIdle()
 {
 	Super::DoIdle();
 
+	// set idle flag
 	isIdle = true;
 
 	if (pathFollowingComp->IsActiveState())
-		return;
+		return; // don't do anything if we're moving already
 
+	// get all staff rooms from the ai manager
 	auto rooms = ACI_AIManager_CPP::GetInstance(GetWorld())->GetRegisteredTile("StaffRoom");
 
 	if (rooms.Num() == 0)
 	{
-		return;
+		return; // can't do anything when there's no staff room, so we just keep standing on our spot
 	}
 
+	// select a random staff room to go to
 	auto goal = rooms[FMath::RandRange(0, rooms.Num() - 1)];
 
+	// get a path to the selected staff room
 	pathFollowingComp->GetNewPath(possessedCustomBasePawn->GetTileCoordinates(), ICI_RegisterAbleInterface_CPP::Execute_GetRegisterLocation(goal));
 }
 
@@ -67,6 +70,8 @@ void ACI_DealerController_CPP::EnterMap()
 
 void ACI_DealerController_CPP::LeaveMap()
 {
+
+	// tell the interactable that he needs a new dealer
 	if (taskInteractable != nullptr)
 	{
 		UCI_InteractableTileData_CPP* dataComp = Cast<UCI_InteractableTileData_CPP>(taskInteractable->GetDataCompRef());
@@ -91,8 +96,9 @@ void ACI_DealerController_CPP::GetNewTask()
 	Super::GetNewTask();
 
 	if (currentTask == nullptr)
-		return;
+		return; // don't do anything if we haven't gotten a new task
 
+	// set references for the new task
 	isIdle = false;
 	taskLocation = currentTask->GetLocation();
 	taskInteractable = currentTask->GetTileLocation();
--- Casino_Inc/Source/Casino_Inc/AI/Characters/CI_DealerController_CPP.h
+++ Casino_Inc/Source/Casino_Inc/AI/Characters/CI_DealerController_CPP.h
@@ -6,10 +6,9 @@
 #include "AI/Base/CI_BaseStaffController_CPP.h"
 #include "CI_DealerController_CPP.generated.h"
 
-//TODO:DOCUMENT comment/document this file
-
 /**
- * 
+ * Controller for the <b>Dealer Controller</b> for Casino Inc.<br>
+ * has all Dealer specific logic
  */
 UCLASS(Blueprintable)
 class CASINO_INC_API ACI_DealerController_CPP : public ACI_BaseStaffController_CPP
@@ -19,20 +18,55 @@ class CASINO_INC_API ACI_DealerController_CPP : public ACI_BaseStaffController_C
 		ACI_DealerController_CPP();
 
 public:
+	/**
+	 *	\brief Function called every frame on this Actor. Override this function to implement custom logic to be executed every frame.
+	 *	\param DeltaSeconds	Game time elapsed during last frame modified by the time dilation
+	 */
 	virtual void Tick(float DeltaSeconds) override;
+
+	/**
+	 * \brief called when we need to get a new task
+	 */
 	virtual void GetNewTask() override;
 
 protected:
+	/**
+	 *\brief native event for when play begins for this actor.
+	 */
 	virtual void BeginPlay() override;
+
+	/**
+	 * \brief implementation for the idle behaviour of the dealer ai
+	 */
 	virtual void DoIdle() override;
+
+	/**
+	 * \brief called when the ai needs to enter the map<br>
+	 * makes the character visible and resets the idle and away booleans
+	 */
 	virtual void EnterMap() override;
+
+	/**
+	 * \brief called when the ai needs to Leave the map<br>
+	 * makes the character move towards a leaving spot and set's the idle and away booleans
+	 */
 	virtual void LeaveMap() override;
 
 public:
+	/**
+	 * \brief c++ implementation for <b>TaskEnded</b>:<br>
+	 * return here if the task has ended
+	 * Defaults to asking for a new task from the ai manager, or go to the idle behavior if none available
+	 * \param success input if the task was a success or a fail
+	 */
 	void TaskEnded_Implementation(bool success) override;
-protected:
 
+protected:
+	// flag for the dealer being idle
 	bool isIdle = true;
+
+	// reference to the task location
 	FVector2D taskLocation;
+	// reference to the task interactable
 	class ACI_BaseTile_CPP* taskInteractable;
 };
